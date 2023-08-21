5d41e677b50ca3c6b3753bc761f9b1ad4f330a48
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-22 01:26:01 +0600

    7 задача без shift

--- 7.js
+++ 7.js
@@ -1,18 +1,21 @@
 const concat = (arr1, arr2) => {
   const resArr = []
 
-  while (arr1.length || arr2.length) {
-    if (arr1.length && !arr2.length) {
-      resArr.push(arr1.shift())
+  let index1 = 0
+  let index2 = 0
+
+  while (index1 < arr1.length || index2 < arr2.length) {
+    if (index2 >= arr2.length) {
+      resArr.push(arr1[index1++])
       continue
     }
 
-    if (!arr1.length && arr2.length) {
-      resArr.push(arr2.shift())
+    if (index1 >= arr1.length) {
+      resArr.push(arr2[index2++])
       continue
     }
 
-    const number = arr1[0] < arr2[0] ? arr1.shift() : arr2.shift()
+    const number = arr1[index1] < arr2[index2] ? arr1[index1++] : arr2[index2++]
 
     resArr.push(number)
   }
@@ -31,4 +34,4 @@ const mergeSort = (arr) => {
   return concat(arr1, arr2)
 }
 
-console.log(mergeSort([10, 45, 3, 2, 1, 0]))
+console.log(mergeSort([10, 45, 3, 0, 2, 1, 10, 11, 19, 10]))
