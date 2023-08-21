71ca1964747641be13650e724e57224b3aa38d33
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-08-21 22:35:49 +0600

    4 задача

---
+++ 4.js
@@ -0,0 +1,38 @@
+const left3 = {
+  value: 4
+}
+
+const right3 = {
+  value: 5,
+}
+
+const left2 = {
+  value: 3,
+  left: left3,
+  right: right3
+}
+
+const left1 = {
+  value: 2,
+  left: left2,
+}
+
+const right1 = {
+  value: 6
+}
+
+const vertex = {
+  value: 1,
+  left: left1,
+  right: right1,
+}
+
+const dfs = (node) => {
+  console.log(node.value)
+
+  if (node.left) dfs(node.left)
+
+  if (node.right) dfs(node.right)
+}
+
+dfs(vertex)
